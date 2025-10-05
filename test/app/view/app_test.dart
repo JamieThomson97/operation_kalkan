0e93650bec5468824caa10f51c035ad47bed7263
Jamie Thomson <jamiethomson97@outlook.com>
2025-10-05 20:14:18 +0100

    tidy up

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,6 +1,6 @@
 // Ignore for testing purposes
-// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:operation_kalkan/app/app.dart';
 import 'package:operation_kalkan/counter/counter.dart';
@@ -8,7 +8,7 @@ import 'package:operation_kalkan/counter/counter.dart';
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(App());
+      await tester.pumpWidget(const ProviderScope(child: App()));
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
--- test/counter/view/counter_page_test.dart
+++ test/counter/view/counter_page_test.dart
@@ -1,6 +1,3 @@
-// Ignore for testing purposes
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
